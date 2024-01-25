172bf2bda8125c7a23c4fe28fd6441c2089ecf9a
name <email>
2024-01-25 14:56:03 +0900

    333

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'http://143.248.197.66:5000',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': '', // '/api'로 시작하는 경로를 타겟 URL에서 제거합니다.
