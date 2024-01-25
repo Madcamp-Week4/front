3cb69719f6220bacc84670fe9c80c7973e8aead9
name <email>
2024-01-25 15:00:12 +0900

    4444

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,15 @@
 import axios from 'axios';
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 axios.defaults.withCredentials = true;
 
-module.exports = function(app) {
-    app.use(
+module.exports = function(App) {
+    App.use(
         '/api',
         createProxyMiddleware({
             target: 'http://143.248.197.66:5000',
             changeOrigin: true,
-            pathRewrite: {
-                '^/api': '', // '/api'로 시작하는 경로를 타겟 URL에서 제거합니다.
-            }
         })
     );
 };
